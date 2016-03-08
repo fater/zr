6cc9add568ba917a0bebc82880d014f8c78ca5e7
Chaikin Evgenii <fat_hack@rambler.ru>
2016-03-08 21:38:54 +0600

    Убрал лишний код

--- public/promo.php
+++ public/promo.php
@@ -3,11 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>За рулем</title>
-    <meta content='width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;' name='viewport' />
-    <meta name="viewport" content="width=device-width" />
     <link rel="stylesheet" href="/public/css/promo.css">
     <script language="JavaScript" src="/public/bower_components/jquery/dist/jquery.min.js"></script>
-    <script language="JavaScript" src="/public/js/promo.js"></script>
 </head>
 <body>
 
